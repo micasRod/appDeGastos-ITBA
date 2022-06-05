6cb1605b1c87c9fa88b0d38e2ad298f26e2c7a75
Mik <rodriguez.mcsol@gmail.com>
2022-06-05 20:32:41 -0300

    añado html y js horribles

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+
 var form=document.getElementById("form");
 var ul=document.getElementById("list");
+var total=0;
+var prom=0;
+var i=0;
 function addLi(){
     var nom=document.getElementById("nombre").value;
     var pre=document.getElementById("precio").value;
     form.addEventListener("submit",function(e){
-        e.preventDefault();
-        var li=document.createElement("li");
-        li.className="list-group-item";
-        li.appendChild(document.createTextNode(nom+": $"+pre))
-        ul.appendChild(li);
+        e.preventDefault(); 
     }) 
+    var li=document.createElement("li");
+    li.className="list-group-item";
+    li.appendChild(document.createTextNode(nom+": $"+pre))
+    ul.appendChild(li);
+    total+=parseInt(pre);
+    i=i+1
+    prom=total/i;
+    //console.log(total,i,prom);
+    
+    document.getElementById("total").innerHTML="Total= $"+ total;
+    document.getElementById("aportes").innerHTML="A cada uno le toca aportar= $"+ prom;
 }
+
+
+
+
 
